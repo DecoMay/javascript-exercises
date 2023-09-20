7ecb4622da407cdc8e9422f938ed21b2647db80b
DecoMay <143063276+DecoMay@users.noreply.github.com>
2023-09-20 15:05:48 +0800

    Complete task

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    text = (text.replace(/\W/g, ''));
+    const palindromeText = Array.from(text.toLowerCase());
+    // console.log(palindromeText);
 
+    for (let i = 1; i < palindromeText.length - i; i++) {
+        if (palindromeText[i - 1] === palindromeText[palindromeText.length - i]) {
+            continue;
+        } else {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
