2658881b921c2de1ae8eaa1ecca0d88544b0b313
DecoMay <143063276+DecoMay@users.noreply.github.com>
2023-09-20 11:22:31 +0800

    Complete task

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(text, num) {
+    let repeatedText = '';
+    if (num >= 0) {
+        for (let i = 0; i < num; i++) {
+            repeatedText += text;
+        }
+    } else {
+        repeatedText = 'ERROR';
+    }
+    return repeatedText;
 };
 
 // Do not edit below this line
