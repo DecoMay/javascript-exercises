871ff3ef862ebc6f4fa346a7f2061277e244e3db
DecoMay <143063276+DecoMay@users.noreply.github.com>
2023-09-20 12:25:36 +0800

    Complete task

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...optionalArgs) {
+    const newArray = [];
+    inputArray.forEach((item) => {
+        if (!optionalArgs.includes(item)) {
+            newArray.push(item);
+        }
+    });
 
+    return newArray;
 };
 
 // Do not edit below this line
