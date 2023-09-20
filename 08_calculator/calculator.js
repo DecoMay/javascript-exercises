73f0865cf0a18a71ab02480433fda8dab7231bec
DecoMay <143063276+DecoMay@users.noreply.github.com>
2023-09-20 14:04:49 +0800

    Complete task

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(firstNum, secondNum) {
+	return firstNum + secondNum;
 };
 
-const subtract = function() {
-	
+const subtract = function(firstNum, secondNum) {
+	return firstNum - secondNum;
 };
 
-const sum = function() {
-	
+const sum = function(numberArray) {
+  let result = 0;
+  numberArray.forEach((item) => result += item);
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(numberArray) {
+  let result = 1;
+  numberArray.forEach((item) => result *= item)
+  return result;
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+	let result = num;
+  for (let i = 0; i < pow - 1; i++) {
+    result *= num;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let result = 1;
+  for (let i = 1; i <= num; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
