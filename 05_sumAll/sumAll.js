a36780c55aab141ba70c928c31b173e2be0aee12
DecoMay <143063276+DecoMay@users.noreply.github.com>
2023-09-20 12:41:20 +0800

    Complete task

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(numberStart, numberEnd) {
+    let output = '';
+    let sum = 0;
+    if ((numberStart >= 0) && (numberEnd >= 0) && (typeof numberStart) === (typeof numberEnd)) {
+        if (numberStart > numberEnd) {
+            for (let i = numberEnd; i <= numberStart; i++) {
+                sum += i;
+            }
+        } else {
+            for (let i = numberStart; i <= numberEnd; i++) {
+                sum += i;
+            }
+        }
+        output = sum;
+    } else {
+        output = 'ERROR';
+    }
+    return output;
 };
 
 // Do not edit below this line
