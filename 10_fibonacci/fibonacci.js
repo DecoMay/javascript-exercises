ec4889f852320a154d9f5a064f93c6dc402f5b30
DecoMay <143063276+DecoMay@users.noreply.github.com>
2023-09-20 15:40:35 +0800

    Complete task

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = parseInt(num);
+    let result = 'OOPS';
+    let f1 = 0;
+    let f2 = 0;
 
+    for (let i = 1; i <= num; i++) {
+        if (i == 1) {
+            result = 1;
+            f1 = 1;
+            continue;
+        }
+        result = f1 + f2;
+        f2 = f1;
+        f1 = result;
+    }
+    return result;
 };
 
 // Do not edit below this line
