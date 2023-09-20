d3ea0c33bfded10a13ef16f1329f7738ac65c456
DecoMay <143063276+DecoMay@users.noreply.github.com>
2023-09-20 16:46:10 +0800

    Complete task

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,22 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(peoples) {
+    return peoples.reduce((oldest, currentPerson) => {
+        // oldestAge gets the age of the oldest person's year of death and birth
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    
+        // currentAge gets the age of the current person's year of death and birth
+        const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+    
+        // return name if current age is older than the oldest age, else return current oldest age
+        return oldestAge < currentAge ? currentPerson : oldest;
+      });
 };
+
+const getAge = function(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+};  
 
 // Do not edit below this line
 module.exports = findTheOldest;
