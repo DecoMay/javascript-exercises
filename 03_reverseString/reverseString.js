4af7a10c946678c0e742f05d74819668e8fda48a
DecoMay <143063276+DecoMay@users.noreply.github.com>
2023-09-20 11:39:17 +0800

    Complete task

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let reversedText = '';
+    for (let i = 0; i <= text.length; i++) {
+        reversedText = text.slice(i, i + 1) + reversedText;
+    }
+    return reversedText;
 };
 
 // Do not edit below this line
