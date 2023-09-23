58c9ab8079ba4ebcb500dc413308aecfc0fa8a4c
DecoMay <143063276+DecoMay@users.noreply.github.com>
2023-09-23 23:48:45 +0800

    Revert

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,16 +1,5 @@
-const palindromes = function (text) {
-    text = (text.replace(/\W/g, ''));
-    const palindromeText = Array.from(text.toLowerCase());
-    // console.log(palindromeText);
+const palindromes = function () {
 
-    for (let i = 1; i < palindromeText.length - i; i++) {
-        if (palindromeText[i - 1] === palindromeText[palindromeText.length - i]) {
-            continue;
-        } else {
-            return false;
-        }
-    }
-    return true;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,20 +1,5 @@
-const fibonacci = function(num) {
-    num = parseInt(num);
-    let result = 'OOPS';
-    let f1 = 0;
-    let f2 = 0;
+const fibonacci = function() {
 
-    for (let i = 1; i <= num; i++) {
-        if (i == 1) {
-            result = 1;
-            f1 = 1;
-            continue;
-        }
-        result = f1 + f2;
-        f2 = f1;
-        f1 = result;
-    }
-    return result;
 };
 
 // Do not edit below this line
