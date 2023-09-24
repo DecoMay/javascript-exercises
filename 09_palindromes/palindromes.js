c605e5208749986395904104f93ecb66c6d1a1fd
DecoMay <143063276+DecoMay@users.noreply.github.com>
2023-09-24 12:05:56 +0800

    Finish task

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    const textArray = text.replace(/\W|_/g, '').toLowerCase();
+    // const textLenght = textArray.length - 1;
+    // let isPalindrome = false;
+    // console.log(textArray);
+    // for (let i = 0; i < textLenght - i; i++) {
+    //     if (textArray[i] === textArray[textLenght - i]) {
+    //         // console.log("Continue");
+    //         isPalindrome = true;
+    //         continue;
+    //     } else {
+    //         // console.log("break")
+    //         isPalindrome = false;
+    //         break;
+    //     }
+    // }
+    // console.log(textArray.split('').reverse().join("") == textArray);
+    return textArray.split('').reverse().join("") == textArray;
 };
 
 // Do not edit below this line
